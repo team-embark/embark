e9cd9086b543a69e2407212be8d0571e43d05977
purple-affogato <ivy.yiting.zhuang@gmail.com>
2025-02-15 11:26:08 -0800

    init apps :>

--- server/init.go
+++ server/init.go
@@ -7,4 +7,19 @@ import (
 
 func initApps() {
 	var err error
+	App, err = firebase.NewApp(Ctx, nil, Opt)
+	if err != nil {
+		fmt.Println("Failed to initiialize firebase app:", err)
+		return
+	}
+	AuthClient, err = App.Auth(Ctx)
+	if err != nil {
+		fmt.Println("Failed to initiialize firebase auth client:", err)
+		return
+	}
+	Client, err = App.Firestore(Ctx)
+	if err != nil {
+		fmt.Println("Failed to initialize firestore client:", err)
+		return
+	}
 }
