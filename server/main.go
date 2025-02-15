6e53f6d279958de5dc5e00697adf130cd35b18c5
al-nf <alankjfung@gmail.com>
2025-02-15 11:16:00 -0800

    begin ,main.go

---
+++ server/main.go
@@ -0,0 +1,41 @@
+package embark
+
+import (
+    "context"
+    "fmt"
+    "net/http"
+
+    firestore "cloud.google.com/go/firestore"
+    firebase "firebase.google.com/go/v4"
+    "firebase.google.com/go/v4/auth"
+    "github.com/rs/cors"
+
+
+)
+
+var Ctx context.Context = context.Background()
+var Opt option.ClientOption = option.WithCredentialsFile("embark.json")
+var (
+    App *firebase.App
+    AuthClient *auth.Client
+    Client *firestore.Client
+)
+
+func main() {
+    mux := &http.ServeMux{}
+
+    mux.HandleFunc("/", helloWorld)
+
+    corsrHandler := cors.New(cors.Options{
+        AllowedOrigins: []string{"*"},
+        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowedHeaders: []string{"Context-Type", "Authorization"},
+        AllowCredentials: true,
+    }).Handler(mux)
+
+    http.ListenAndServe(":8080", corsHandler)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello World!")
+}
