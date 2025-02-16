f3f7f2f974355133b2401cc08864b9ebf1b7b6ba
Carys Chiu <caryschiu@carcputer.local>
2025-02-15 18:01:39 -0800

    simple banner added

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Button from "./map/button.svelte";
+	let { children } = $props();
+</script>
+
+
+<div class="banner">
+  <nav>
+	<a href="/">home</a>
+	<a href="/">map</a>
+    EMBARK
+    <a href="/signin">Sign Out</a>
+</nav>
+</div>
+
+<style>
+    .banner {
+        background:rgb(193, 122, 67);
+        padding: 20px;
+        color: white;
+        font-size: 1.5rem;
+        text-align: center;
+        border-radius: 4px;
+    }
+</style>
+
+{@render children()}
+
