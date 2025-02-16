57bab79eecc83f3ef1e914589b26772a82988062
al-nf <alankjfung@gmail.com>
2025-02-15 23:37:46 -0800

    added get for singular park

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -13,6 +13,7 @@
                     // This gives you a Google Access Token. You can use it to access the Google API.
                     const credential = GoogleAuthProvider.credentialFromResult(result);
                     const token = credential.accessToken;
+                    console.log(token);
                     // The signed-in user info.
                     const user = result.user;
                     
