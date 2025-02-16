cf1cdbcc45325415e3a2dcf7c5e03509beb84716
mschemell <rita.shchmel@gmail.com>
2025-02-15 22:01:23 -0800

    qr code done

---
+++ client/src/routes/qrcode/NewTestComponent.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let text = "Fun fact!";
+  </script>
+  
+  <p>{text}</p>
+  
+  <style>
+    p {
+      color: rgb(255, 255, 255);
+      font-family: Arial, sans-serif;
+      font-size: 1.5em;
+      text-align: center;
+      position: absolute;
+      top: 400px;  /* Moves text up or down */
+      left: 700px; /* Moves text left or right */
+    }
+  </style>
